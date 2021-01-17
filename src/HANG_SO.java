347f22b88781353597610e3a645da45b95885bf1
thaonoucamp <dangxuanthaoht@gamil.com>
2021-01-17 21:11:32 +0700

    add

---
+++ src/HANG_SO.java
@@ -0,0 +1,14 @@
+public class HANG_SO {
+    public static final int CONSTANT = 4;
+
+    public static final float PI = 3.14f;
+
+    public static final String JAVA = "Java";
+
+    public static void main(String[] args) {
+        System.out.println("Hello " + JAVA);
+
+        float area = PI * 5;
+        System.out.println("Area: " + area);
+    }
+}
